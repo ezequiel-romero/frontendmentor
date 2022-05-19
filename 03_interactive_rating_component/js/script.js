66a444d4ab61f8ac4e352a9406727f5e3fdac7e8
ezequiel-romero <eze.1003@gmail.com>
2022-05-19 15:40:10 -0300

    Update solution

--- 03_interactive_rating_component/js/script.js
+++ 03_interactive_rating_component/js/script.js
@@ -1,23 +1,11 @@
-var btns = document.querySelectorAll(".rating-number");
-var btnActive = '';
+let form = document.querySelector('#form-rate');
+let flipCard = document.querySelector('.flip-card-inner');
+let rateGiven = document.querySelector('.rate-given');
 
-for (var i = 0; i < btns.length; i++) {
-    btns[i].addEventListener("click", function() {
-        var current = document.querySelector(".active");
-        if (current) {
-            current.className = current.className.replace(" active", "");
-        }
-        this.className += " active";
-        return btnActive = this.textContent;
-    });
-  }
-
-document.querySelector(".submit").addEventListener("click", function() {
-    if (document.querySelector(".active")) {
-        document.querySelector(".rating").textContent = 'You selected ' + btnActive + ' out of 5';
-        document.querySelector(".rating-container").className += " d-none";
-        document.querySelector(".thankyou-container").className += " d-block";
-    } else {
-        alert('Please choose a rating!')
-    }
-});
+form.onsubmit = e => {
+    e.preventDefault();
+    const formData = new FormData(form);
+    const formRate = formData.get('rating');
+    rateGiven.textContent = formRate;
+    flipCard.className += " flipped";
+}
